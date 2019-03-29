0c2516f67ec0a0a934a354bb2686281a73a5eb47
ZXF982214487 <982214487@qq.com>
2019-03-29 13:09:27 +0800

    Update day.py

--- trailbreaker/日历/day.py
+++ trailbreaker/日历/day.py
@@ -12,16 +12,16 @@ import framebuf
 usrspi = USR_SPI(scl=Pin('X6',Pin.OUT_PP), sda=Pin('X7', Pin.OUT),dc=Pin('X8', Pin.OUT))
 disp = DISPLAY(usrspi,cs=Pin('X5', Pin.OUT),res=Pin('X4', Pin.OUT),led_en=Pin('X3', Pin.OUT))
 
-disp.clr(disp.WHITE)
-disp.putstr_back(0,2,"01",disp.BLACK,disp.BLUE)
+disp.clr(disp.WHITE)#清屏
+disp.putstr_back(0,2,"01",disp.BLACK,disp.BLUE)#用蓝色显示周一到周五
 disp.putstr_back(3,2,"02",disp.BLACK,disp.BLUE)
 disp.putstr_back(6,2,"03",disp.BLACK,disp.BLUE)
 disp.putstr_back(9,2,"04",disp.BLACK,disp.BLUE)
 disp.putstr_back(12,2,"05",disp.BLACK,disp.BLUE)
-disp.putstr_back(15,2,"06",disp.BLACK,disp.RED)
+disp.putstr_back(15,2,"06",disp.BLACK,disp.RED)#红色显示周六周日
 disp.putstr_back(18,2,"07",disp.BLACK,disp.RED)
 
-disp.putstr_back(0,4,"01",disp.BLACK,disp.YELLOW)
+disp.putstr_back(0,4,"01",disp.BLACK,disp.YELLOW)#黄色显示日期 1-30
 disp.putstr_back(3,4,"02",disp.BLACK,disp.YELLOW)
 disp.putstr_back(6,4,"03",disp.BLACK,disp.YELLOW)
 disp.putstr_back(9,4,"04",disp.BLACK,disp.YELLOW)
@@ -52,7 +52,7 @@ disp.putstr_back(18,10,"28",disp.BLACK,disp.YELLOW)
 disp.putstr_back(0,12,"29",disp.BLACK,disp.YELLOW)
 disp.putstr_back(3,12,"30",disp.BLACK,disp.YELLOW)
 
-disp.put_hline(0,13,121,disp.BLACK)
+disp.put_hline(0,13,121,disp.BLACK)#黑色线画网格
 disp.put_hline(0,31,121,disp.BLACK)
 disp.put_hline(0,49,121,disp.BLACK)
 disp.put_hline(0,67,121,disp.BLACK)
@@ -67,7 +67,7 @@ disp.put_vline(87,13,108,disp.BLACK)
 disp.put_vline(105,13,108,disp.BLACK)
 
 
-rtc = pyb.RTC()
+rtc = pyb.RTC()#rtc获取当前时间
 rtc.datetime((2014, 5, 1, 4, 13, 0, 0, 0))
 a=rtc.datetime()
 t1=a[0]
@@ -79,7 +79,7 @@ t6=a[5]
 t7=a[6]
 x=(a[3]-1)%7*3
 y=((a[3]-1)//7+2)*2
-if a[3]>=10:
+if a[3]>=10:#将当前日期用粉色标记出来
 	disp.putstr_back(x,y,str(a[3]),disp.WHITE,disp.WHITE)
 	disp.putstr_back(x,y,str(a[3]),disp.BLACK,disp.PINK)
 else:
@@ -95,12 +95,12 @@ if t6>=10:
 	s5='time:'+str(t5)+':'+str(t6)+':'+str(t7)
 else:
 	s5='time:'+str(t5)+': '+str(t6)+':'+str(t7)
-disp.putstr(0,14,s1,disp.BLACK)
-disp.putstr(11,14,s2,disp.BLACK)
-disp.putstr(0,15,s3,disp.BLACK)
-disp.putstr(11,15,s4,disp.BLACK)
-disp.putstr(0,16,s5,disp.BLACK)
-while True:
+disp.putstr(0,14,s1,disp.BLACK)#显示“年”信息
+disp.putstr(11,14,s2,disp.BLACK)#显示“月”信息
+disp.putstr(0,15,s3,disp.BLACK)#显示“日”信息
+disp.putstr(11,15,s4,disp.BLACK)#显示“星期”信息
+disp.putstr(0,16,s5,disp.BLACK)#显示“时间”信息
+while True:#显示时间变化
 	print(rtc.datetime())
 	a=rtc.datetime()
 
