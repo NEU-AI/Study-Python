7d8a20883264f053434e41785600d20aecc13ec7
yangzl2014 <yangshenghello66@163.com>
2019-04-22 22:19:18 +0800

    修改代码中的命名不合理的地方，提交缺少的umqtt.py

--- skids/mqtt互发表情/pics.py
+++ skids/mqtt互发表情/pics.py
@@ -1,3 +1,5 @@
+
+
 import ubitmap
 import screen
 import time
@@ -5,7 +7,7 @@ import text
 import network
 from machine import UART,Pin
 import utime
-from umqtt.simple import MQTTClient
+from umqtt import MQTTClient
 screen.clear()
 
 class pics():
@@ -13,24 +15,35 @@ class pics():
 		self.keys = [Pin(p, Pin.IN) for p in [35, 36, 39, 34]]
 		self.keymatch = ["Key1", "Key2", "Key3", "Key4"]
 		self.select=1
-		self.displayInit()
+		self.contentisplayInit()
 		self.wifi_name = "NEUI"
+
 		self.wifi_SSID = "NEUI3187"
 		#MQTT服务端信息
-		self.SERVER = "112.125.89.85"   #MQTT服务器地址
-		self.SERVER_PORT = 3881         #MQTT服务器端口
-		self.DEVICE_ID = "wc001"        #设备ID
-		self.TOPIC1 = b"/cloud-skids/online/dev/" + self.DEVICE_ID
-		self.TOPIC2 = b"/cloud-skids/message/server/" + self.DEVICE_ID
-		self.CLIENT_ID = "f25410646a8348f8a1726a3890ad8f73"
+		self.SERVER = "192.168.5.121"   #MQTT服务器地址
+		self.SERVER_PORT = 1883         #MQTT服务器端口
+		self.contentEVICE_ID = "wc001"        #设备ID
+		self.TOPIC1 = b"/cloud-skids/online/dev/" + self.contentEVICE_ID
+		self.TOPIC2 = b"/cloud-skids/message/server/" + self.contentEVICE_ID
+		self.CLIENT_ID = "7e035cd4-15b4-4d4b-a706-abdb8151c57d"
 		self.uart = UART(1, baudrate=115200, bits=8, parity=0, rx=18, tx=19, stop=1)
 		#设备状态
 		self.ON = "1"
 		self.OFF = "0"
-		self.d=" "#初始化要发送的信息
-		self.c = MQTTClient(self.CLIENT_ID, self.SERVER, self.SERVER_PORT)#定义一个mqtt实例
+
+
+		self.content=" "#初始化要发送的信息
+
+
+		self.client = MQTTClient(self.CLIENT_ID, self.SERVER, self.SERVER_PORT)#定义一个mqtt实例
+
+
 	def drawInterface(self):#界面初始化
+
+
 		bmp1=ubitmap.BitmapFromFile("pic/boy")
+
+
 		bmp2=ubitmap.BitmapFromFile("pic/girl")
 		bmp1.draw(20,200)#显示boy图片
 		bmp2.draw(140,200)#显示girl图片
@@ -56,14 +69,14 @@ class pics():
 		screen.drawline(20, 272, 20, 200, 2, 0xff0000)
 	def displayInit(self):#初始化
 		screen.clear()
-		self.drawInterface()
+		self.contentrawInterface()
 		self.selectInit()
 	def esp(self):
-		self.c.set_callback(self.sub_cb)    #设置回调
-		self.c.connect()
+		self.client.set_callback(self.sub_cb)    #设置回调
+		self.client.connect()
 		print("连接到服务器：%s" % self.SERVER)
-		self.c.publish(self.TOPIC1, self.ON)     #发布“1”到TOPIC1
-		self.c.subscribe(self.TOPIC2)       #订阅TOPIC
+		self.client.publish(self.TOPIC1, self.ON)     #发布“1”到TOPIC1
+		self.client.subscribe(self.TOPIC2)       #订阅TOPIC
 		#display.text("从微信取得信息", 20, 20, 0xf000, 0xffff)
 	
 	
@@ -94,14 +107,28 @@ class pics():
 		if self.keymatch[key] == "Key3":#发送表情按键
 			if self.select%2==1:#显示已发送boy表情
 				bmp1=ubitmap.BitmapFromFile("pic/boy")
+
+
 				bmp1.draw(140,40)
-				self.d="001"
-				self.c.publish(self.TOPIC2,self.d)#给服务器发送boy表情的号码
+
+
+				self.content="001"
+
+
+				self.client.publish(self.TOPIC2,self.content)#给服务器发送boy表情的号码
+
+
 			else:#显示已发送girl表情
+
+
 				bmp2=ubitmap.BitmapFromFile("pic/girl")
+
+
 				bmp2.draw(140,40)
-				self.d="002"
-				self.c.publish(self.TOPIC2,self.d)#给服务器发送girl表情的号码
+
+
+				self.content="002"
+				self.client.publish(self.TOPIC2,self.content)#给服务器发送girl表情的号码
 	def sub_cb(self,topic, message):#从服务器接受信息
 		message = message.decode()
 		print("服务器发来信息：%s" % message)
@@ -118,11 +145,12 @@ class pics():
 	def start(self):
 		try:
 			while True:
-				self.c.check_msg()#检查是否收到信息
+				self.client.check_msg()#检查是否收到信息
 				i = 0#用来辅助判断那个按键被按下
 				j = -1
 				for k in self.keys:#检查按键是否被按下
 					if (k.value() == 0):##如果按键被按下
+
 						if i != j:
 							j = i
 							self.keyboardEvent(i)#触发相应按键对应事件
@@ -131,7 +159,7 @@ class pics():
 						i = 0
 				time.sleep_ms(130)
 		finally:
-				self.c.disconnect()
+				self.client.disconnect()
 				print("MQTT连接断开")
 			
 if __name__ == '__main__':
@@ -145,3 +173,4 @@ if __name__ == '__main__':
 			
 			
 			
+
